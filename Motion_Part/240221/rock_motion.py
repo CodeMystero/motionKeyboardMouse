33dc77ff342747fc252312013017ca982929f315
yonggarigari <149138812+yonggarigari@users.noreply.github.com>
2024-02-21 16:12:16 +0900

    Update rock_motion.py

--- Motion_Part/240221/rock_motion.py
+++ Motion_Part/240221/rock_motion.py
@@ -11,6 +11,28 @@ fingers = None
 def distance(p1, p2):
     return math.dist((p1.x, p1.y), (p2.x, p2.y))  # 두 점 p1, p2의 x, y 좌표로 거리를 계산한다.
 
+# 세 점 사이의 각도 계산 함수 선언
+def calculate_angle(p1, p2, p3):   
+# 벡터 계산
+    v1 = (p1.x - p2.x, p1.y - p2.y)
+    v2 = (p3.x - p2.x, p3.y - p2.y)
+
+# 벡터의 크기 계산
+    v1_mag = math.sqrt(v1[0]**2 + v1[1]**2)
+    v2_mag = math.sqrt(v2[0]**2 + v2[1]**2)
+
+# 내적 계산
+    dot_product = v1[0] * v2[0] + v1[1] * v2[1]
+
+# 코사인 값 계산
+    cosine_angle = dot_product / (v1_mag * v2_mag)
+
+# 각도 계산 (라디안)
+    angle_rad = math.acos(cosine_angle)
+
+# 라디안 값을 각도로 변환하여 반환
+    angle =math.degrees(angle_rad)
+
 # 손 좌표 인식 함수 선언
 def dect_hand(image):
     global points
@@ -70,6 +92,12 @@ while True:  # 무한 반복
     dect_hand(image)
     dect_finger(points)
 
+    if points is not None:
+        print(calculate_angle(points[6], points[7], points[5]))
+        
+
+
+    # 주먹 인식
     if fingers == 0:
         hand_shape = "rock"
         cv2.putText(  # 인식된 내용을 이미지에 출력한다.
@@ -81,6 +109,8 @@ while True:  # 무한 반복
             (0, 255, 0),
             1
         )
+
+    # 마우스 움직이기 & 클릭
     elif fingers == 2:
         if distance(points[8], points[0]) > distance(points[7], points[0]):
             if distance(points[4], points[0]) > distance(points[3], points[0]):
@@ -97,16 +127,16 @@ while True:  # 무한 반복
                 y = int(points[8].y * frame_height)
 
                 pyautogui.moveTo(x, y)
+                
+                print(distance(points[8], points[4])*100)
+                if distance(points[8], points[4]) < 10:
+                    pyautogui.click()
+                
         
-    
-
-
-
-
 
     cv2.imshow("MediaPipe Hands", frame)
 
-    key = cv2.waitKey(5) & 0xFF  # 키보드 입력받기
+    key = cv2.waitKey(1) & 0xFF  # 키보드 입력받기
     if key == 27:  # ESC를 눌렀을 경우
         break  # 반복문 종료
 
